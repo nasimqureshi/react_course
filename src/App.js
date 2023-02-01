341c273282a51fb6bf2ace2186c3ddafe06d5bf7
nasimqureshi <qureshi.nasim@yahoo.com>
2023-01-31 22:09:34 -0600

    commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 // import logo from './logo.svg';
 import './App.css';
-import Header from './Header';
-import Footer from './Footer';
+import Person from './Components/Person';
+import Man from './Components/Man';
 
 function App() {
-  let name = "Nasim Qureshi"
+  
   return (
     <div className='App'>
-      <header className='App-header'>
-        <h1>I am a react APP</h1>
-      </header>
+      <Person></Person>
+      <Man></Man>
     </div>
   );
 }
---
+++ src/Components/Person.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class Person extends Component {
+   // constructor() {}
+    render() {
+       return (
+        <div>
+           <div>Name: Nasim Qureshi</div>
+           <div>Age: 69 Years</div>
+        </div>
+       )
+
+    }
+}
+export default Person;
