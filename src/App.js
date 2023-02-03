0b284cf85f9b9d1c5031fcccacdfb9d386f76816
nasimqureshi <qureshi.nasim@yahoo.com>
2023-02-02 22:04:52 -0600

    commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 // import logo from './logo.svg';
 // import './App.css';
 import Person from './Components/Person';
+import Demo from './Demo';
 // import Man from './Components/Man';
 
 function App() {
   
   return (
     <div className='App'>
+      <Demo number= "1"></Demo>
       <Person name= "Nasim" age ="69" />
+      <Demo number= "2"></Demo>
       <Person name= "Qamar"  age = "57"/>
+      <Demo number= "3"></Demo>
       <Person name= "Samreen" age = "41" />
      
     </div>
